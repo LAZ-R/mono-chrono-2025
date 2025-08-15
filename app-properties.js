ebf1bd741a6de89ba63d6574d90e3a52310dc925
LAZ-R <crazydrawer62@gmail.com>
2025-08-15 16:59:02 +0200

    v 0.0.2 - fix html titles

--- app-properties.js
+++ app-properties.js
@@ -1,6 +1,6 @@
 export const APP_NAME = 'MONO CHRONO';
-export const APP_VERSION = '0.0.1';
+export const APP_VERSION = '0.0.2';
 export const APP_LOCAL_STORAGE_ID = `mono-chrono-2025-001`;
 
 // CHANGELOG
-// v 0.0.1 - initialisation
+// v 0.0.2 - fix html titles
